f0303ba923653e6132aeb28e12b07636511caf8d
Diana Cioroiu <dianacioroiu20@gmail.com>
2023-07-09 14:48:58 +0300

    A new Commit!

--- Main.java
+++ Main.java
@@ -5,5 +5,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Version control");
         System.out.println("Commit");
+        System.out.println("New Commit");
     }
 }
